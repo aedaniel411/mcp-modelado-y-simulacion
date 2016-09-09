d0224e7af60d17e7759e2b8338347a4f3a207e91
Daniel Acosta <aedaniel@gmail.com>
2016-09-09 10:43:55 -0500

    Con cuda
    Con implementación en cuda

--- juego_de_la_vida_conway/src/game_life.cu
+++ juego_de_la_vida_conway/src/game_life.cu
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <string.h>
 #include <time.h>
 #include <ncurses.h>
 
+#include <curand_kernel.h>
+#include <curand.h>
+
 //#define DELAY 600000
 #define DELAY 1
 
-void inicializar (int *mat, int rows, int cols) {
-	for (int i = 0; i < rows; i++) {			
-		for (int j = 0; j < cols; j++ ) {
-			mat[i * cols + j] = rand() % 2;
-		}
-	}
+__global__ void inicializar (int *mat, int rows, int cols, unsigned int seed) {
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	int ren = blockIdx.y * blockDim.y + threadIdx.y;
+	int index = ren * cols + col;
+
+	curandState_t state;
+	curand_init (seed, index, 0, &state);
+
+	mat[index] = curand (&state) % 2;
 }
 
 void mostrar (int *mat, int rows, int cols) {
 	char cBlock = (char)0x2588;
-	
-	for (int i = 0; i < rows; i++) {			
+
+	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++ ) {
-			if (mat[i * cols + j] == 1) { 
-				mvaddch(i, j, cBlock);			
+			if (mat[i * cols + j] == 1) {
+				mvaddch(i, j, cBlock);
 			}
 		}
 	}
 }
 
-int sumvivos (int *mat, int row, int rows, int col, int cols) {
+__device__ int sumvivos (int *mat, int row, int rows, int col, int cols) {
 	int sum = 0;
-	
-	for (int r = (row - 1); r <= row + 1; r++) { 
+
+	for (int r = (row - 1); r <= row + 1; r++) {
 		for (int c = col - 1; c <= col + 1; c++) {
 			if (!((r == row) && (c == col))) {
 				if ((r >= 0) && (r < rows)) {
@@ -41,11 +48,11 @@ int sumvivos (int *mat, int row, int rows, int col, int cols) {
 			}
 		}
 	}
-	
-	return sum;
-} 
 
-int viveomuere (int actual, int vivos) {
+	return sum;
+}
+
+__device__ int viveomuere (int actual, int vivos) {
 	int siguiente = 0;
 	if (actual == 0) {
 		if (vivos == 3) {				//Nacimiento
@@ -60,58 +67,82 @@ int viveomuere (int actual, int vivos) {
 			siguiente = 0;				//muerte por superpoblacion
 		}
 	}
-	
+
 	return siguiente;
 }
 
-int main() {	
-	int rows, cols, sum = 0;
-	
-	srand (time(NULL)); 
-	
-	initscr();									
-	curs_set(FALSE); 							
-	
-	getmaxyx(stdscr, rows, cols); 				
-	rows = rows - 1;
-			
-	int *matA, *matB; 
-	matA = (int *)calloc( cols * rows, sizeof(int));
-	
-	inicializar (matA, rows, cols);
-	
-	mostrar (matA, rows, cols);
-		
-	refresh();									
-	getch();
-	
-	int g = 1;
-	while (1) {
-		
-		matB = (int *)calloc( cols * rows, sizeof(int));
-		
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j ++) {
-				sum = sumvivos (matA, i, rows, j, cols);
-				
-				matB [i * cols + j] = viveomuere (matA [i * cols + j], sum);
-			}
-		}
-		
-		free(matA);
-		matA = matB;
+__global__ void nuevaGeneracon (int *matOLD, int *matNEW, int rows, int cols) {
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	int ren = blockIdx.y * blockDim.y + threadIdx.y;
+	int index = ren * cols + col;
+	int sum = 0;
 
+	sum = sumvivos (matOLD, ren, rows, col, cols);
+	matNEW[index] = viveomuere(matOLD[index], sum);
+}
+
+int cuantosBloques(int N, int hilos) {
+	int c, m = N % hilos;
+	if (m > 0) {
+		c = (N + hilos) / hilos;
+	} else {
+		c = N / hilos;
+	}
+	return c;
+}
+
+int main() {
+	int rows, cols; //, sum = 0;
+
+	initscr();
+	curs_set(FALSE);
+
+	getmaxyx(stdscr, rows, cols);
+	rows = rows - 1;
+
+	int *matA;
+	matA = (int *)calloc( cols * rows, sizeof(int));
+
+	int *dmatA, *dmatB;
+	cudaMalloc((void**)&dmatA, cols * rows * sizeof(int));
+
+	dim3 dimThreadsBloque(16, 16);
+
+	dim3 dimBloques(cuantosBloques(cols,16), cuantosBloques(rows,16));
+
+	inicializar <<<dimBloques, dimThreadsBloque>>> (dmatA, rows, cols, time(NULL));
+
+	cudaMemcpy(matA, dmatA, cols * rows * sizeof(int), cudaMemcpyDeviceToHost);
+
+	mostrar (matA, rows, cols);
+
+	refresh();
+	getch();
+
+	int g = 1;
+
+	while (1) {
+		cudaMalloc((void**)&dmatB, cols * rows * sizeof(int));
+
+		nuevaGeneracon <<<dimBloques, dimThreadsBloque>>> (dmatA, dmatB, rows, cols);
+
+		cudaMemcpy(matA, dmatB, cols * rows * sizeof(int), cudaMemcpyDeviceToHost);
+
+		dmatA = dmatB;
+        cudaFree (dmatB);
 		clear();
 		mostrar (matA, rows, cols);
 		mvprintw (rows, 1, "Generacion %d", g++);
-		refresh();			
-								
+		refresh();
+
 //		usleep(DELAY);
-		sleep(DELAY);	
-		
+		sleep(DELAY);
+
 	}
-	
-	free (matB);
+
+	free (matA);
+	cudaFree(dmatA);
+
 	endwin();
 	return 0;
 }
